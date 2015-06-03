b49d597d8d796bfa2f0e6194bc1d7f145ea9f0e6
Luis Lopez <iam@3rdlof.com>
2015-06-03 13:05:11 -0700

    Created a quick Piwik plugin
    Based on the latest version of Piwik because the other available don not work.

--- plugin.php
+++ plugin.php
@@ -1,25 +1,35 @@
 <?php
 /*
-Plugin Name: Your Plugin Name
-Plugin URI: http://github.com/your_name/your_plugin
-Description: One line description of your plugin
+Plugin Name: yourls-quick-piwik
+Plugin URI: https://github.com/3rdLOF/yourls-quick-piwik
+Description: Simple piwik plugin
 Version: 1.0
-Author: Your Name
-Author URI: http://your-site-if-any/
+Author: 3rdLOF
+Author URI: http://shorty.3rdlof.com/zme93
 */
 
 // No direct call
 if( !defined( 'YOURLS_ABSPATH' ) ) die();
 
-/*
+yourls_add_action( 'html_head', 'trackWithPiwik' );
 
- Your code goes here.
- 
- Suggested read:
- https://github.com/YOURLS/YOURLS/wiki/Coding-Standards
- https://github.com/YOURLS/YOURLS/wiki#for-developpers
- https://github.com/YOURLS/YOURLS/wiki/Plugin-List#get-your-plugin-listed-here
- 
- Have fun!
- 
-*/
+function trackWithPiwik() {
+        echo "<script type=\"text/javascript\">
+
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u='//ADD YOUR PIWIK INSTALLATION URL HERE/';
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', ADD YOUR SITE ID HERE]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+
+</script>       ";
+        echo "<noscript><p><img src=\"//ADD YOUR PIWIK INSTALLATION URL HERE/piwik.php?idsite=ADD YOUR SITE ID HERE\" style=\"border:0;\" alt=\"\" /></p></noscript>" ;
+
+}
+?>
+
